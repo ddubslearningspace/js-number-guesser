f924474f07b599dc7c58163b7e68973fa84a788b
Daniel Warner <52971033+ddubthedev@users.noreply.github.com>
2020-09-08 16:54:48 -0600

    added basic functinality for a win

---
+++ app.js
@@ -0,0 +1,47 @@
+// Game Variables
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+      
+// Assign UI Min and Max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function(){
+  // reset eror message
+  setMessage("", "inherit")
+
+  let guess = parseInt(guessInput.value);
+
+  // Validate
+  if(isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if win
+  if(guess === winningNum) {
+    // disable input
+    guessInput.disabled = true;
+    // change border color
+    guessInput.style.borderColor = 'green';
+    // Set message
+    setMessage(`WINNER! ${winningNum} is correct.`, 'green');
+  } else {
+
+  }
+});
+
+function setMessage(msg, color = "black") {
+  message.style.color = color;
+  message.textContent = msg;
+}
